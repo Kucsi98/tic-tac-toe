fc6408ca52f12655592d367b7d9ed0590876e864
Kucsi98 <kucsko.zsofi@gmail.com>
2024-06-19 14:41:01 +0200

    tic-tac-toe is done

---
+++ src/Game.jsx
@@ -0,0 +1,13 @@
+import Board from "./Board";
+import "./App.css";
+
+const Game = () => {
+  return (
+    <div className="game">
+      Tic-Tac-Toe
+      <Board />
+    </div>
+  );
+};
+
+export default Game;
